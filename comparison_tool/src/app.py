7ae07126df6b4f120faa9445f6064af49de80855
Amol J <102950660+amol776@users.noreply.github.com>
2025-06-03 09:09:27 +0530

    Update app.py
    test Revert

--- comparison_tool/src/app.py
+++ comparison_tool/src/app.py
@@ -12,7 +12,7 @@ from utils.comparison_engine import ComparisonEngine
 from reports.report_generator import ReportGenerator
 
 # Constants
-SUPPORTED_SOURCES = [s
+SUPPORTED_SOURCES = [
     'CSV file',
     'DAT file',
     'SQL Server',
